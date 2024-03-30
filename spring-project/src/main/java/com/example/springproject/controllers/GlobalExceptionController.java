7b17c1d45101760bb2d3618fd229fcbf63d2905a
Samarth <samarthrn19@gmail.com>
2024-03-30 18:57:11 +0530

    Implemented Global exception handler class using @ControllerAdvice and @ExceptionHandler

---
+++ spring-project/src/main/java/com/example/springproject/controllers/GlobalExceptionController.java
@@ -0,0 +1,19 @@
+package com.example.springproject.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(Exception exception){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        modelAndView.addObject("errorMessage", exception.getMessage());
+        return modelAndView;
+    }
+}
